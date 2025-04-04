493d967423771707c466ff6297feb13ac0e3bf7a
vickyK <kangjiawei@KANGKANGDELL>
2025-04-04 15:50:52 +1300

    Added class and services

---
+++ WebAPI-ControllerBase-Practice1/Models/Pizza.cs
@@ -0,0 +1,9 @@
+﻿namespace WebAPI_ControllerBase_Practice1.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsGlutenFree { get; set; }
+    }
+}
